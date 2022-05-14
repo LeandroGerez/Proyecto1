46cd86aba9d5a1a8742ef9349b80eab1e8b40288
leandrogerez <leandrogerez8@gmail.com>
2022-05-14 12:49:02 -0300

    Borrar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 import React from 'react'
 import logo from './logo.svg';
-import './App.css';
+
+import NavBar from './components/NavBar/NavBar';
+ 
 
 
-const AvatarJSX = (props) => {
-  console.log(props)
-  const src = `https://randomuser.me/api/portraits/lego/${props.id}.jpg`
 
-  return (
-<picture>
-<img src={src} alt='lego 1'/> 
-{props.name}
-
-
-</picture>
-  )
-}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      <AvatarJSX id={2} name="Leonel" />
-      <AvatarJSX id={3} name="Leandro" />
-      </header>
+      
+        <NavBar />
     </div>
   );
 }
