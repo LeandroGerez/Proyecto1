7125ef6e910f7c49921edb2afc99832194cd2678
leandrogerez <leandrogerez8@gmail.com>
2022-05-14 04:46:25 -0300

    Clase de webpack

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+
+
+const AvatarJSX = (props) => {
+  console.log(props)
+  const src = `https://randomuser.me/api/portraits/lego/${props.id}.jpg`
+
+  return (
+<picture>
+<img src={src} alt='lego 1'/> 
+{props.name}
+
+
+</picture>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <AvatarJSX id={2} name="Leonel" />
+      <AvatarJSX id={3} name="Leandro" />
       </header>
     </div>
   );
