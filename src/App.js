187eb52ad759a5114260fbd7a9530448afc140c8
leandrogerez <leandrogerez8@gmail.com>
2022-05-16 00:28:34 -0300

    Habemus Gerez

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import logo from './logo.svg';
-
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import CountFunction from './components/CountFunction/CountFunction';
 import NavBar from './components/NavBar/NavBar';
- 
+import CartWidget from './components/CartWidget/CartWidget';
 
 
 
@@ -10,8 +10,12 @@ import NavBar from './components/NavBar/NavBar';
 function App() {
   return (
     <div className="App">
-      
+      <header className="App-header">
         <NavBar />
+        <CartWidget/>
+        <CountFunction/>
+        <ItemListContainer greeting ="Bienvenidos a Habemus"/>
+      </header>
     </div>
   );
 }
