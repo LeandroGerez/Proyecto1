248baac3a57c4d46b77f32cf4a5595594927baca
leandrogerez <leandrogerez8@gmail.com>
2022-05-25 18:54:54 -0300

    Desafio Item

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,6 +1,23 @@
-const Item = ({id,name,price}) =>{
+import './Item.css'
+
+
+const Item = ({id,name,price,img,category,description}) =>{
 return(
+    <div className='Item'>
     <h1>{name}</h1>
+<img
+src={img} alt='img'
+
+/>
+<h2>{description}</h2>
+
+
+    <p>{category}</p>
+    
+    
+    <p>{price}</p>
+    <button>More details</button>
+    </div>
 )
 }
 
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,10 +1,12 @@
 import Item from "../Item/Item"
 
 const ItemList = ({products}) => {
+    
 
     return(
         <div>
             {products.map(prod => <Item key={prod.id} {...prod}/>)}
+            
         </div>
     )
 
